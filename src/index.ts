1d9024353a2ce5e700952ab2767a3a3d446a8d6e
stscoundrel <sampo@pixels.fi>
2021-02-20 20:22:18 +0200

    Add markWords function + test cases

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
-export const example = (): string => 'test string';
+export const markWords = (search: string, content: string): string => {
+  if (search === undefined || content === undefined) throw new Error('Missing argument: expects two strings as function arguments');
+
+  return content.split(search).join(`<mark>${search}</mark>`);
+};
 
 export default {
-  example,
+  markWords,
 };
---
+++ tests/markari.test.ts
@@ -0,0 +1,36 @@
+import { markWords } from '../src';
+
+describe('Markari tests', () => {
+  test('Throws error on missing argument', () => {
+    expect(() => {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      markWords(undefined as any, undefined as any);
+    }).toThrow('Missing argument: expects two strings as function arguments');
+  });
+
+  test('Returns a string', () => {
+    const original = 'And if the day would only come, then you might just appear, even though you\'d soon be gone';
+    const result = markWords('Lorem', original);
+    expect(typeof result === 'string').toBeTruthy();
+  });
+
+  test('Does not edit string if no matches', () => {
+    const original = 'And if the day would only come, then you might just appear, even though you\'d soon be gone';
+    const result = markWords('Not in the sentence', original);
+    expect(result).toEqual(original);
+  });
+
+  test('Adds markup if a match is found', () => {
+    const original = 'And if the day would only come, then you might just appear, even though you\'d soon be gone';
+    const expected = 'And if the <mark>day</mark> would only come, then you might just appear, even though you\'d soon be gone';
+    const result = markWords('day', original);
+    expect(result).toEqual(expected);
+  });
+
+  test('Adds markup for all instances of the word', () => {
+    const original = 'And if the day would only come, then you might just appear, even though you\'d soon be gone';
+    const expected = 'And if the day would only come, then <mark>you</mark> might just appear, even though <mark>you</mark>\'d soon be gone';
+    const result = markWords('you', original);
+    expect(result).toEqual(expected);
+  });
+});
