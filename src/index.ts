dd8a5bc152c76e28ad12a1113036828f7731f2f8
stscoundrel <sampo@pixels.fi>
2021-05-13 10:56:51 +0300

    Dont format by empty search keyword

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 export const markWords = (search: string, content: string): string => {
   if (search === undefined || content === undefined) throw new Error('Missing argument: expects two strings as function arguments');
 
+  if (!search) {
+    return content;
+  }
+
   return content.split(search).join(`<mark>${search}</mark>`);
 };
 
